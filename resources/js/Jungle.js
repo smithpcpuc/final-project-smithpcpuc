a525a4b9586ad44f34dc72b00d06e2d6d9e05ec8
Erich Wagner <erswagner@gmail.com>
2020-11-15 20:07:52 -0500

    Hello

---
+++ resources/js/Jungle.js
@@ -0,0 +1,31 @@
+var config = {
+    type: JungleGame.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new JungleGame.Game(config);
+
+function preload ()
+{
+    this.load.image('jungle', 'assets/junglebackground.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.spritesheet('guy', 'assets/JungleGuy-v3.png', { frameWidth: 100, frameHeight: 92 });
+}
+
+function create ()
+{
+
+}
+
+function update ()
+{
+
+}
