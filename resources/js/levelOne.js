2482be58f4d80a7bb671782ef73fc275afa60808
Nicholas-Lawson <lawsonnj@mail.uc.edu>
2020-12-02 23:08:13 -0500

    bug fix

--- resources/js/levelOne.js
+++ resources/js/levelOne.js
@@ -110,7 +110,7 @@ function create ()
     bombs = this.physics.add.group();
 
     //  The score
-    scoreText = this.add.text(16, 16, 'SCORE: 0', { fontSize: '32px', fill: '#ff0000' });
+    scoreText = this.add.text(16, 16, 'SCORE: ' + score, { fontSize: '32px', fill: '#ff0000' });
 
     //  Collide the player and the stars with the platforms
     this.physics.add.collider(player, platforms);
